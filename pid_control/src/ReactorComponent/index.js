30f0cfaa2f27cf019a303df48726df276fee65c5
MilaGabs <50213361+MilaGabs@users.noreply.github.com>
2020-11-08 12:05:14 -0300

    Desenho img tanque

--- pid_control/src/ReactorComponent/index.js
+++ pid_control/src/ReactorComponent/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 
 const ReactorComponent = (props) =>{
 
     const {model} = props
 
+    var canvas = document.getElementById('canvas');
+
+    function desenhaImagem(){
+        var ctx = canvas.getContext("2d");
+        var tanque = new Image();
+        tanque.src = "tanque.png";
+
+        ctx.drawImage(tanque, 0, 0, 500, 500);
+    }
+
+    useEffect(()=>{
+        if (canvas)
+        desenhaImagem();
+    },[])
+
+    
     return (
         <React.Fragment>
             REACTOR
+            <canvas id="canvas" width="500" height="500"/>
         </React.Fragment>
     )
 }
