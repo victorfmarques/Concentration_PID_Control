baa01fb67b3ebd9cfa548200ee7f43fa0098cf25
lelebs <legabatel@hotmail.com>
2020-11-08 12:25:36 -0300

    Initial Commit Forms

--- pid_control/src/App.js
+++ pid_control/src/App.js
@@ -1,18 +1,21 @@
 import FormComponent from './FormComponent'
 import ReactorComponent from './ReactorComponent'
 import ChartComponent from './ChartComponent'
+import ReactorModel from './ReactorModel';
 
 function App() {
+  var viewModel = new ReactorModel(
+    {p: 0, i: 0, d: 0}
+  );
+  
   return (
     <div className="AppWrapper">
-      <FormComponent/>
+      <FormComponent viewModel={viewModel} />
       <div className="GraphicalWrapper">
         <ReactorComponent/>
         <ChartComponent/>
       </div>
-      
     </div>
-    
   );
 }
 
--- pid_control/src/ReactorModel.js
+++ pid_control/src/ReactorModel.js
@@ -1,10 +1,13 @@
 class ReactorModel {
-
     constructor (data){
-        p = data.p;
-        i = data.i;
-        d = data.d;
+        this.p = data.p;
+        this.i = data.i;
+        this.d = data.d;
     }
+
+    p;
+    i;
+    d;
 }
 
 export default ReactorModel
